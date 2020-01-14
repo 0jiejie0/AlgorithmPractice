4449a6f5f8efc9b95b4775d033d9a423d6af7834
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-01-14 19:04:01 +0800

    测试代码&项目文件

---
+++ src/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new HashMap<Integer, Integer>().get(2));
+    }
+}
