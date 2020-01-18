1f64dc84bc25a02d0cf65e9047762e76a362ff3d
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-01-18 16:58:36 +0800

    连续数组 解题思路二-优化一实现（效率更垃圾了！！！）

--- src/main/leetcode_20200117/Solution.java
+++ src/main/leetcode_20200117/Solution.java
@@ -23,12 +23,12 @@ public class Solution {
      * 3、终点从起点位置+1开始，length-1位置（末尾）结束
      * 4、终点每次挪动两个位置
      * 5、每次挪动终点累加计算新加入计算范围的两个元素，比较计算范围长度与历史最长子数组长度，计算范围长则更新记录
-     * 解题思路二：
+     * 解题思路二：[1158 ms]
      * 1、在全长范围内找到最长子数组（仅限定偶数个元素，不限定01等量）的和以及长度
      * 2、依次检查定长子数组，根据出入数组元素调整当前数组和
      * 3、子数组和为0，符合条件，返回；否则不符合条件，继续检查下一个数组
      * 4、当前长度的所有子数组检查均不合条件，将长度缩短，继续搜索
-     * 优化一：
+     * 优化一：[1308 ms]
      * 1、当前范围的01差量限制了最长子数组长度<=当前范围长度-差量
      *
      * @param nums
@@ -45,12 +45,21 @@ public class Solution {
             maxSum += nums[i];
         }
         int length = nums.length;
-        if (length % 2 == 0) {
-            maxLength = findMaxLength(nums, maxSum, length);
+        if (maxSum > 0) {//优化一，maxSum绝对值代表了01量差，从length中去除这一部分
+            length -= maxSum;
         } else {
-            length--;
-            maxLength = findMaxLength(nums, maxSum - nums[length], length);
+            length += maxSum;
         }
+        for (int i = length; i < nums.length; i++) {
+            maxSum -= nums[i];//从总和中去除多余部分的值
+        }
+        maxLength = findMaxLength(nums, maxSum, length);
+//        if (length % 2 == 0) {
+//            maxLength = findMaxLength(nums, maxSum, length);
+//        } else {
+//            length--;
+//            maxLength = findMaxLength(nums, maxSum - nums[length], length);
+//        }
 //        for (int start = 0; start < nums.length - 1; start++) {
 //            nowSum = 0;
 //            for (int end = start + 1; end < nums.length; end += 2) {
