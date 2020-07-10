36f9997f2d003e3af3a1e2ef5e612672c388fd31
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-10 10:48:43 +0800

    leetcode第21题，合并两个有序链表，类设计不规范使得单元测试调试花费太多时间

---
+++ src/test/problemAndSolving/leetcode_20200709/SolutionTest.java
@@ -0,0 +1,25 @@
+package test.problemAndSolving.leetcode_20200709;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200709.ListNode;
+import main.problemAndSolving.leetcode_20200709.Solution;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testMergeTwoLists() {
+        ListNode head = solution.mergeTwoLists(
+                ListNode.createList(new int[]{1, 2, 4}),
+                ListNode.createList(new int[]{1, 3, 4})
+        );
+        System.out.println(head.toString());
+    }
+}
