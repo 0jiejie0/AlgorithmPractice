cac8a2d1b8211f355f4574929a4a53f188e6892f
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-11 23:19:31 +0800

    leetcode第28题，实现strStr方法（一定长度的字符串中找出第一次完整匹配出给定字符串的下标）循环的编写能力有待提升

---
+++ src/test/problemAndSolving/leetcode_20200711/SolutionTest.java
@@ -0,0 +1,21 @@
+package test.problemAndSolving.leetcode_20200711;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200711.Solution;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testStrStr() {
+        Assert.assertEquals(2, solution.strStr("hello", "ll"));
+    }
+}
