44b94aa014e20b4f32a6fb650bf892a02f51ea79
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-14 23:31:30 +0800

    leetcode第38题，外观数列

---
+++ src/test/problemAndSolving/leetcode_20200714/SolutionTest.java
@@ -0,0 +1,26 @@
+package test.problemAndSolving.leetcode_20200714;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200714.Solution;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testCountAndSay() {
+        Assert.assertEquals("1", solution.countAndSay(1));
+        Assert.assertEquals("11", solution.countAndSay(2));
+        Assert.assertEquals("21", solution.countAndSay(3));
+        Assert.assertEquals("1211", solution.countAndSay(4));
+        Assert.assertEquals("111221", solution.countAndSay(5));
+        Assert.assertEquals("312211", solution.countAndSay(6));
+    }
+}
