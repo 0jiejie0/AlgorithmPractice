203d8f654daf20d2b9b939813d9b32668d6a2fe0
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-18 22:21:37 +0800

    leetcode第53题，最大子序和，这道题目标签是简单的，但实际上并不简单，甚至有点挫伤信心，可能在动规啥的算是简单的吧

---
+++ src/test/problemAndSolving/leetcode_20200715/SolutionTest.java
@@ -0,0 +1,24 @@
+package test.problemAndSolving.leetcode_20200715;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200715.Solution;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testMaxSubArray() {
+        Assert.assertEquals(6, solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        Assert.assertEquals(-1, solution.maxSubArray(new int[]{-2, -1}));
+        Assert.assertEquals(-2, solution.maxSubArray(new int[]{-2, -2}));
+        Assert.assertEquals(-1, solution.maxSubArray(new int[]{-1, -2}));
+    }
+}
