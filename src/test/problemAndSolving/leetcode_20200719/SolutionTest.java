8a3bd8763133bd24dd56a8f32ec9b179a63bab55
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-20 00:30:32 +0800

    leetcode第66题,在表示数字的数组中+1，易出bug

---
+++ src/test/problemAndSolving/leetcode_20200719/SolutionTest.java
@@ -0,0 +1,23 @@
+package test.problemAndSolving.leetcode_20200719;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200719.Solution;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testPlusOne() {
+        Assert.assertArrayEquals(new int[]{1}, solution.plusOne(new int[]{0}));
+        Assert.assertArrayEquals(new int[]{1, 2, 4}, solution.plusOne(new int[]{1, 2, 3}));
+        Assert.assertArrayEquals(new int[]{1, 0}, solution.plusOne(new int[]{9}));
+    }
+}
