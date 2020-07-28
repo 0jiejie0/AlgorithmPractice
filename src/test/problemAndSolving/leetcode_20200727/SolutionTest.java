76e0ed943f4c3dbef7e1139db18b63e175f843f4
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-07-28 19:11:38 +0800

    leetcode第67题，二进制求和

---
+++ src/test/problemAndSolving/leetcode_20200727/SolutionTest.java
@@ -0,0 +1,22 @@
+package test.problemAndSolving.leetcode_20200727;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200727.Solution;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+    Solution solution;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solution = new Solution();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAddBinary() {
+        Assert.assertEquals("100", solution.addBinary("11", "1"));
+        Assert.assertEquals("10101", solution.addBinary("1010", "1011"));
+    }
+}
