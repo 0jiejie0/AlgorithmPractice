e39c2d26381f60ee6a377c6bf8d4159a1da70496
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-06 23:34:47 +0800

    leetcode第100题，相同的树

---
+++ src/main/problemAndSolving/leetcode_20200806/Solution.java
@@ -0,0 +1,91 @@
+package main.problemAndSolving.leetcode_20200806;
+//给定两个二叉树，编写一个函数来检验它们是否相同。
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+//
+// 示例 1:
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true
+//
+// 示例 2:
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+//
+//
+// 示例 3:
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+//
+// Related Topics 树 深度优先搜索
+//本来想做两道题，看来是不行了，
+// 虽然标签提示用深搜，但是我想用宽搜用队列绕开递归，但是提交运行时报类型转换异常，看样子进队元素需要是Comparable子类
+//java.lang.ClassCastException: class TreeNode cannot be cast to class java.lang.Comparable (TreeNode is in unnamed module of loader 'app'; java.lang.Comparable is in module java.base of loader 'bootstrap')
+//换到栈改用深搜，想尝试递推解决深搜问题，考虑到最后发现需要处理的情况很多，尤其是所有结点的值相同的情况无法实现，且简单分析发现此法不如递归简洁高效，
+// 最后改用递归解决
+// 用时88m
+//执行耗时:0 ms,击败了100.00% 的Java用户
+//		内存消耗:37.1 MB,击败了79.25% 的Java用户
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return judgeUseStack(p, q);
+    }
+
+    private boolean judgeUseStack(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {//pq均不为null，且值不相等时为不同树
+            if (p.val != q.val) {
+                return false;
+            }
+        } else if (p == q) {//pq均为null
+            return true;
+        } else {//pq有一个为null，不同树
+            return false;
+        }
+        return judgeUseStack(p.left, q.left) & judgeUseStack(p.right, q.right);
+    }
+
+    private boolean judgeUseQueue(TreeNode p, TreeNode q) {
+        PriorityQueue<TreeNode> l = new PriorityQueue<TreeNode>();
+        PriorityQueue<TreeNode> r = new PriorityQueue<TreeNode>();
+        while (p != null && q != null) {
+            if (p.val != q.val) {//结点值不同，为不同树
+                return false;
+            }
+            if (p.left != null && q.left != null) {//两个结点均有左子，入队
+                l.add(p.left);
+                r.add(q.left);
+            } else if (p.left != q.left) {//只有一个结点有左子，为不同树
+                return false;
+            }
+            if (p.right != null && q.right != null) {
+                l.add(p.right);
+                r.add(q.right);
+            } else if (p.right != q.right) {
+                return false;
+            }
+            p = l.poll();
+            q = r.poll();
+        }
+        return p == q;
+    }
+}
---
+++ src/test/problemAndSolving/leetcode_20200806/SolutionTest.java
@@ -0,0 +1,21 @@
+package test.problemAndSolving.leetcode_20200806;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20200806.Solution;
+import main.problemAndSolving.leetcode_20200806.TreeNode;
+import org.junit.Assert;
+
+public class SolutionTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testIsSameTree() {
+        Assert.assertEquals(true, new Solution().isSameTree(new TreeNode(1), new TreeNode(1)));
+        Assert.assertEquals(false, new Solution().isSameTree(new TreeNode(1), new TreeNode(2)));
+    }
+}
