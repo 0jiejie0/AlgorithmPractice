08dc81bd2863f9cef9b929b06e2b8cb8697267d9
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-08 00:10:10 +0800

    leetcode第100题，相同的树，增加宽搜实现 因为原来使用了优先级队列PriorityQueue，这种队列要求入队元素满足一定可比要求（至少是Comparable子类对象），因而提交出错，换用Linklist解决问题

--- src/main/problemAndSolving/leetcode_20200806/Solution.java
+++ src/main/problemAndSolving/leetcode_20200806/Solution.java
@@ -38,17 +38,19 @@ package main.problemAndSolving.leetcode_20200806;
 //本来想做两道题，看来是不行了，
 // 虽然标签提示用深搜，但是我想用宽搜用队列绕开递归，但是提交运行时报类型转换异常，看样子进队元素需要是Comparable子类
 //java.lang.ClassCastException: class TreeNode cannot be cast to class java.lang.Comparable (TreeNode is in unnamed module of loader 'app'; java.lang.Comparable is in module java.base of loader 'bootstrap')
+//###这里是因为使用了优先级队列PriorityQueue，这种队列要求入队元素满足一定可比要求（至少是Comparable子类对象），换用Linklist解决问题
 //换到栈改用深搜，想尝试递推解决深搜问题，考虑到最后发现需要处理的情况很多，尤其是所有结点的值相同的情况，条件复杂，简单分析发现此法不如递归简洁高效，
 // 最后改用递归解决
 // 用时88m
 //执行耗时:0 ms,击败了100.00% 的Java用户
 //		内存消耗:37.1 MB,击败了79.25% 的Java用户
 
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return judgeUseStack(p, q);
+        return judgeUseQueue(p, q);
     }
 
     private boolean judgeUseStack(TreeNode p, TreeNode q) {
@@ -64,9 +66,18 @@ public class Solution {
         return judgeUseStack(p.left, q.left) & judgeUseStack(p.right, q.right);
     }
 
+    /**
+     * 原来的队列对象创建有问题，修改后可以实现
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:37.2 MB,击败了52.72% 的Java用户
+     *
+     * @param p
+     * @param q
+     * @return
+     */
     private boolean judgeUseQueue(TreeNode p, TreeNode q) {
-        PriorityQueue<TreeNode> l = new PriorityQueue<TreeNode>();
-        PriorityQueue<TreeNode> r = new PriorityQueue<TreeNode>();
+        Queue<TreeNode> l = new LinkedList<>();
+        Queue<TreeNode> r = new LinkedList<>();
         while (p != null && q != null) {
             if (p.val != q.val) {//结点值不同，为不同树
                 return false;
