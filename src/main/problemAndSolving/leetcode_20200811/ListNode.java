b9fd335d862b073ce08b4d866f7692bb2f9224f4
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-11 23:40:30 +0800

    leetcode第141题，环形链表

---
+++ src/main/problemAndSolving/leetcode_20200811/ListNode.java
@@ -0,0 +1,11 @@
+package main.problemAndSolving.leetcode_20200811;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
--- src/main/problemAndSolving/leetcode_20200811/Solution.java
+++ src/main/problemAndSolving/leetcode_20200811/Solution.java
@@ -57,4 +57,61 @@ public class Solution {
         }
         return res;
     }
+
+    //给定一个链表，判断链表中是否有环。
+//
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+//
+//
+//
+// 示例 1：
+//
+// 输入：head = [3,2,0,-4], pos = 1
+//输出：true
+//解释：链表中有一个环，其尾部连接到第二个节点。
+//
+//
+//
+//
+// 示例 2：
+//
+// 输入：head = [1,2], pos = 0
+//输出：true
+//解释：链表中有一个环，其尾部连接到第一个节点。
+//
+//
+//
+//
+// 示例 3：
+//
+// 输入：head = [1], pos = -1
+//输出：false
+//解释：链表中没有环。
+//
+//
+//
+//
+//
+//
+// 进阶：
+//
+// 你能用 O(1)（即，常量）内存解决此问题吗？
+// Related Topics 链表 双指针
+// 快慢指针，存在环时两个指针一定能相遇（快指针跑过一圈后追上慢指针），用时14m
+// 执行耗时:0 ms,击败了100.00% 的Java用户
+//		内存消耗:40.1 MB,击败了32.00% 的Java用户
+    public boolean hasCycle(ListNode head) {
+        ListNode low = head, fast = head;
+        if (fast != null) {
+            fast = fast.next;
+        }
+        while (fast != null && fast.next != null) {
+            if (low.val == fast.val) {
+                return true;
+            }
+            low = low.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
 }
