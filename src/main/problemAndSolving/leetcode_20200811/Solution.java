89cd2f78381ecf1d500b94e265e4abb44e5d5c21
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-11 23:21:39 +0800

    leetcode第136题，只出现一次的数字

--- src/main/problemAndSolving/leetcode_20200811/Solution.java
+++ src/main/problemAndSolving/leetcode_20200811/Solution.java
@@ -29,4 +29,32 @@ public class Solution {
         sum -= root.val;
         return root.left == root.right && sum == 0 || hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
+
+    //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 说明：
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+//
+// 示例 1:
+//
+// 输入: [2,2,1]
+//输出: 1
+//
+//
+// 示例 2:
+//
+// 输入: [4,1,2,1,2]
+//输出: 4
+// Related Topics 位运算 哈希表
+// 对蓝桥讲的内容有些印象，同数一次异或运算结果为0，耗时1m47s
+// 执行耗时:1 ms,击败了99.67% 的Java用户
+//		内存消耗:40.7 MB,击败了74.62% 的Java用户
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
 }
