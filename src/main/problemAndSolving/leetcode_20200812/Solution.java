ae45132648969e266310f1e908b63e21e8acf821
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-12 23:26:10 +0800

    leetcode第118题，杨辉三角

--- src/main/problemAndSolving/leetcode_20200812/Solution.java
+++ src/main/problemAndSolving/leetcode_20200812/Solution.java
@@ -14,9 +14,13 @@ package main.problemAndSolving.leetcode_20200812;
 // 输入: "race a car"
 //输出: false
 //
-// Related Topics 双指针 字符串 还算比较简单，
+// Related Topics 双指针 字符串
+// 还算比较简单，只是做着题突然idea崩了，解题用时没法发算了
 // 执行耗时:3 ms,击败了92.53% 的Java用户
 //		内存消耗:40.1 MB,击败了23.24% 的Java用户
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Solution {
     public boolean isPalindrome(String s) {
@@ -42,5 +46,49 @@ public class Solution {
             }
         }
         return true;
+    }
+
+    //给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+//
+//
+//
+// 在杨辉三角中，每个数是它左上方和右上方的数的和。
+//
+// 示例:
+//
+// 输入: 5
+//输出:
+//[
+//     [1],
+//    [1,1],
+//   [1,2,1],
+//  [1,3,3,1],
+// [1,4,6,4,1]
+//]
+// Related Topics 数组
+// 漏了行数为0的边界条件，用时12m
+// 执行耗时:1 ms,击败了77.34% 的Java用户
+//		内存消耗:37.1 MB,击败了93.23% 的Java用户
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (numRows == 0) {
+            return res;
+        }
+        List<Integer> line = new LinkedList<>();
+        List<Integer> last;
+        line.add(1);
+        res.add(line);
+        last = line;
+        for (int i = 1; i < numRows; i++) {
+            line = new LinkedList<>();
+            line.add(1);
+            for (int j = 1; j < last.size(); j++) {
+                line.add(last.get(j - 1) + last.get(j));
+            }
+            line.add(1);
+            res.add(line);
+            last = line;
+        }
+        return res;
     }
 }
