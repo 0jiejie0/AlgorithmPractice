cc26dd9222390b0e1d46b12634105ebe7adc2dad
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-08-15 21:56:16 +0800

    leetcode第155题，最小栈优化

--- src/main/problemAndSolving/leetcode_20200815/MinStack.java
+++ src/main/problemAndSolving/leetcode_20200815/MinStack.java
@@ -41,38 +41,80 @@ package main.problemAndSolving.leetcode_20200815;
 // 借助LinkList写起来比较简单，但是性能不够好，用时17m
 // 执行耗时:7 ms,击败了78.91% 的Java用户
 //		内存消耗:41.7 MB,击败了25.12% 的Java用户
-
-import java.util.LinkedList;
-import java.util.List;
+//
+//
+// 用自己写的链表，性能提升效果并不明显
+//
+//  执行耗时:6 ms,击败了97.18% 的Java用户
+//		内存消耗:41.5 MB,击败了58.40% 的Java用户
+//	执行耗时:5 ms,击败了99.92% 的Java用户
+//		内存消耗:41.5 MB,击败了61.81% 的Java用户
+//	执行耗时:6 ms,击败了97.18% 的Java用户
+//		内存消耗:41.6 MB,击败了50.12% 的Java用户
 
 public class MinStack {
-    List<Integer> data;
-    List<Integer> min;
+    class Node {
+        int val;
+        Node last;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node last) {
+            this.val = val;
+            this.last = last;
+        }
+
+        public Node() {
+        }
+    }
+
+    //    List<Integer> data;
+//    List<Integer> min;
+    int size;
+    Node data;
+    Node min;
 
     public MinStack() {
-        data = new LinkedList<>();
-        min = new LinkedList<>();
+//        data = new LinkedList<>();
+//        min = new LinkedList<>();
+        size = 0;
+        data = new Node();
+        min = new Node();
     }
 
     public void push(int x) {
-        data.add(x);
-        if (min.size() == 0 || min.get(min.size() - 1) >= x) {
-            min.add(x);
+//        data.add(x);
+//        if (min.size() == 0 || min.get(min.size() - 1) >= x) {
+//            min.add(x);
+//        }
+        data = new Node(x, data);
+        if (size == 0 || min.val >= x) {
+            min = new Node(x, min);
         }
+        size++;
     }
 
     public void pop() {
-        int a = data.remove(data.size() - 1);
-        if (a == min.get(min.size() - 1)) {
-            min.remove(min.size() - 1);
+//        int a = data.remove(data.size() - 1);
+//        if (a == min.get(min.size() - 1)) {
+//            min.remove(min.size() - 1);
+//        }
+        if (data.val == min.val) {
+            min = min.last;
         }
+        data = data.last;
+        size--;
     }
 
     public int top() {
-        return data.get(data.size() - 1);
+//        return data.get(data.size() - 1);
+        return data.val;
     }
 
     public int getMin() {
-        return min.get(min.size() - 1);
+//        return min.get(min.size() - 1);
+        return min.val;
     }
 }
