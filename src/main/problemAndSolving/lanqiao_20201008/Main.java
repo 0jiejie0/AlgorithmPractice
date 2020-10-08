68c31839acc928c0da4957f400f0b8544625a3b4
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-10-08 20:49:05 +0800

    蓝桥入门训练 斐波那契数列，没有看之前的代码，犹豫了一点时间，一遍过，尚可

--- src/main/problemAndSolving/lanqiao_20201008/Main.java
+++ src/main/problemAndSolving/lanqiao_20201008/Main.java
@@ -47,7 +47,53 @@ public class Main {
         return BigInteger.valueOf(n).multiply(BigInteger.valueOf(m));
     }
 
+    /**
+     * 试题 入门训练 Fibonacci数列
+     * <p>
+     * 资源限制
+     * 时间限制：1.0s   内存限制：256.0MB
+     * 问题描述
+     * Fibonacci数列的递推公式为：Fn=Fn-1+Fn-2，其中F1=F2=1。
+     * <p>
+     * 当n比较大时，Fn也非常大，现在我们想知道，Fn除以10007的余数是多少。
+     * <p>
+     * 输入格式
+     * 输入包含一个整数n。
+     * 输出格式
+     * 输出一行，包含一个整数，表示Fn除以10007的余数。
+     * 说明：在本题中，答案是要求Fn除以10007的余数，因此我们只要能算出这个余数即可，而不需要先计算出Fn的准确值，再将计算的结果除以10007取余数，直接计算余数往往比先算出原数再取余简单。
+     * <p>
+     * 样例输入
+     * 10
+     * 样例输出
+     * 55
+     * 样例输入
+     * 22
+     * 样例输出
+     * 7704
+     * 数据规模与约定
+     * 1 <= n <= 1,000,000。
+     *
+     * @param n
+     * @return
+     */
+    public static final int fibo(int n) {
+        int mod = 10007;
+        int res = 1, last = 1;
+        for (int i = 3; i <= n; i++) {
+            res ^= last;
+            last ^= res;
+            res ^= last;
+            res += last;
+            if (res > mod) {
+                res %= mod;
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(sum(new Scanner(System.in).nextInt()));
+//        System.out.println(sum(new Scanner(System.in).nextInt()));
+        System.out.println(fibo(new Scanner(System.in).nextInt()));
     }
 }
