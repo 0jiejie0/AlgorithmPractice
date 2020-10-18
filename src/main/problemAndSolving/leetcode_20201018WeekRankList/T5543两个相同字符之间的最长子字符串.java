2d34f82a0486a69ee1a383fcdd1daa02a7f87406
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-10-18 18:49:22 +0800

    leetcode周赛，只做出来一道

---
+++ src/main/problemAndSolving/leetcode_20201018WeekRankList/T5543两个相同字符之间的最长子字符串.java
@@ -0,0 +1,54 @@
+package main.problemAndSolving.leetcode_20201018WeekRankList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//给你一个字符串 s，请你返回 两个相同字符之间的最长子字符串的长度 ，计算长度时不含这两个字符。如果不存在这样的子字符串，返回 -1 。
+//
+//子字符串 是字符串中的一个连续字符序列。
+//
+//
+//
+//示例 1：
+//
+//输入：s = "aa"
+//输出：0
+//解释：最优的子字符串是两个 'a' 之间的空子字符串。
+//示例 2：
+//
+//输入：s = "abca"
+//输出：2
+//解释：最优的子字符串是 "bc" 。
+//示例 3：
+//
+//输入：s = "cbzxy"
+//输出：-1
+//解释：s 中不存在出现出现两次的字符，所以返回 -1 。
+//示例 4：
+//
+//输入：s = "cabbac"
+//输出：4
+//解释：最优的子字符串是 "abba" ，其他的非最优解包括 "bb" 和 "" 。
+//
+//
+//提示：
+//
+//1 <= s.length <= 300
+//s 只含小写英文字母
+public class T5543两个相同字符之间的最长子字符串 {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        int res = -1;
+        char c;
+        for (int i = 0; i < chars.length; i++) {
+            c = chars[i];
+            if (map.containsKey(c)) {//计算当前字符与第一次出现位置的距离，并保存结果
+                res = (i - map.get(c) - 1) > res ? (i - map.get(c) - 1) : res;
+            } else {
+                map.put(c, i);//记录字符首次出现位置
+            }
+        }
+        return res;
+    }
+}
