1b57bf4ac9812db1347ba08c35f65fd4d7715910
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-10-18 19:36:31 +0800

    leetcode周赛，思路有问题，关于冲突球员的取舍没有找到合适的数据结构，未解

---
+++ src/main/problemAndSolving/leetcode_20201018WeekRankList/T5545无矛盾的最佳球队.java
@@ -0,0 +1,88 @@
+package main.problemAndSolving.leetcode_20201018WeekRankList;
+
+//假设你是球队的经理。对于即将到来的锦标赛，你想组合一支总体得分最高的球队。球队的得分是球队中所有球员的分数 总和 。
+//
+//然而，球队中的矛盾会限制球员的发挥，所以必须选出一支 没有矛盾 的球队。如果一名年龄较小球员的分数 严格大于 一名年龄较大的球员，则存在矛盾。同龄球员之间不会发生矛盾。
+//
+//给你两个列表 scores 和 ages，其中每组 scores[i] 和 ages[i] 表示第 i 名球员的分数和年龄。请你返回 所有可能的无矛盾球队中得分最高那支的分数 。
+//
+//
+//
+//示例 1：
+//
+//输入：scores = [1,3,5,10,15], ages = [1,2,3,4,5]
+//输出：34
+//解释：你可以选中所有球员。
+//示例 2：
+//
+//输入：scores = [4,5,6,5], ages = [2,1,2,1]
+//输出：16
+//解释：最佳的选择是后 3 名球员。注意，你可以选中多个同龄球员。
+//示例 3：
+//
+//输入：scores = [1,2,3,5], ages = [8,9,10,1]
+//输出：6
+//解释：最佳的选择是前 3 名球员。
+//
+//
+//提示：
+//
+//1 <= scores.length, ages.length <= 1000
+//scores.length == ages.length
+//1 <= scores[i] <= 106
+//1 <= ages[i] <= 1000
+public class T5545无矛盾的最佳球队 {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        TreeNode node = new TreeNode(scores[0], ages[0]);
+        for (int i = 1; i < scores.length; i++) {
+            node.put(scores[i], ages[i]);
+        }
+        return 0;//思路不行，解决不了。。。
+    }
+}
+
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int score;
+    int age;
+
+    public TreeNode(int score, int age) {
+        this.score = score;
+        this.age = age;
+    }
+
+    public static final boolean compare(TreeNode a, TreeNode b) {
+        //1.a.score>b || 2.a.score==b && a.age>=b
+        if (a.score > b.score) {
+            return true;
+        } else if (a.score == b.score && a.age >= b.age) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean betterThan(TreeNode node) {
+        return compare(this, node);
+    }
+
+    public void put(TreeNode node) {
+        if (betterThan(node)) {
+            if (right == null) {
+                right = node;
+            } else {
+                right.put(node);
+            }
+        } else {
+            if (left == null) {
+                left = node;
+            } else {
+                left.put(node);
+            }
+        }
+    }
+
+    public void put(int score, int age) {
+        put(new TreeNode(score, age));
+    }
+}
