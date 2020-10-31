5ef3fcd79735437cef1e0a0cb59e2724310aacd2
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-10-31 20:30:55 +0800

    leetcode周赛，依照惯例通过两道题，按键时间利用长度26的数组映射；等差数组规模不大，没有好的思路直接暴力

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,20 +1,25 @@
 package test;
 
-import java.util.LinkedList;
-
 public class Test {
     public static void main(String[] args) {
 //        System.out.println(new HashMap<Integer, Integer>().get(2));
-        //测试foreach中list改变对遍历内容的影响
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.add(23);
-        linkedList.add(2563);
-        linkedList.add(2);
-        for (Integer i : linkedList) {//列表改动之后，此处会报ConcurrentModificationException
-            if (linkedList.size() == 3) {
-                linkedList.remove(1);
-            }
-            System.out.println(i);
+
+//        //测试foreach中list改变对遍历内容的影响
+//        LinkedList<Integer> linkedList = new LinkedList<>();
+//        linkedList.add(23);
+//        linkedList.add(2563);
+//        linkedList.add(2);
+//        for (Integer i : linkedList) {//列表改动之后，此处会报ConcurrentModificationException
+//            if (linkedList.size() == 3) {
+//                linkedList.remove(1);
+//            }
+//            System.out.println(i);
+//        }
+
+        //测试boolean数组的默认值（应该是false）
+        boolean[]booleans=new boolean[2];
+        for (int i = 0; i < booleans.length; i++) {
+            System.out.println(booleans[i]);//false
         }
     }
 }
