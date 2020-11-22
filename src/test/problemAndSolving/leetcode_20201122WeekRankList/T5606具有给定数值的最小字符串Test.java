452e1f941824c0e3d50b768f3580316c42abc488
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-11-22 18:44:59 +0800

    leetcode周赛，费了半天劲通过了两个没有任何思维含量的题目

---
+++ src/test/problemAndSolving/leetcode_20201122WeekRankList/T5606具有给定数值的最小字符串Test.java
@@ -0,0 +1,23 @@
+package test.problemAndSolving.leetcode_20201122WeekRankList;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20201122WeekRankList.T5606具有给定数值的最小字符串;
+
+public class T5606具有给定数值的最小字符串Test extends TestCase {
+    T5606具有给定数值的最小字符串 exam;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        exam = new T5606具有给定数值的最小字符串();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetSmallestString() {
+//        assertEquals("aaabz", exam.getSmallestString(5, 31));
+//        assertEquals("aay", exam.getSmallestString(3, 27));
+        assertEquals("aaszz", exam.getSmallestString(5, 73));
+//        assertEquals("aaszz", exam.getSmallestString(74657, 743771));
+    }
+}
