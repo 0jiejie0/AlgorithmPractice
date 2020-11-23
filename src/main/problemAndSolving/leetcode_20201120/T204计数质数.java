ecf08cefa4ec8d02394fbb84fb3aefd94184137f
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2020-11-23 20:55:03 +0800

    leetcode第204题，计数质数，简化筛法代码，简化代码效率提升一倍

--- src/main/problemAndSolving/leetcode_20201120/T204计数质数.java
+++ src/main/problemAndSolving/leetcode_20201120/T204计数质数.java
@@ -44,21 +44,26 @@ package main.problemAndSolving.leetcode_20201120;
 //		内存消耗:35.6 MB,击败了88.56% 的Java用户
 //		执行耗时:75 ms,击败了16.45% 的Java用户
 //		内存消耗:35.6 MB,击败了89.21% 的Java用户
+
+// 果然是筛法代码有问题，简化代码效率提升了一倍
+// 执行耗时:48 ms,击败了20.41% 的Java用户
+//		内存消耗:35.3 MB,击败了95.39% 的Java用户
+// 执行耗时:48 ms,击败了20.41% 的Java用户
+//		内存消耗:35.9 MB,击败了84.28% 的Java用户
+
 public class T204计数质数 {
     public int countPrimes(int n) {
         if (n <= 3) {
             return n == 3 ? 1 : 0;
         }
-        byte[] counts = new byte[n / 8 + 1];
+        byte[] counts = new byte[n / 8 + 1];//这里申请的空间位数至少为n+1位，有空余，所以舍弃第一位，下标即数字，减少差一的运算量
         int res = 0;
-        for (int i = 1; i < n - 1; i++) {
-            if ((counts[i / 8] & (1 << (i % 8))) == 0) {
+        for (int i = 2; i < n; i++) {
+            if ((counts[i / 8] & (1 << (i & 7))) == 0) {
                 res++;
-                i++;
-                for (int j = 2 * i - 1; j < n; j += i) {
-                    counts[j / 8] |= (1 << (j % 8));
+                for (int j = i << 1; j < n; j += i) {//此处用移位代替乘法操作似乎并不快
+                    counts[j / 8] |= (1 << (j & 7));
                 }
-                i--;
             }
         }
         return res;
