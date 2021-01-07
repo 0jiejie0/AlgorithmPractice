006213ad30d1b71ff52671af1eff1fc91e16ddc7
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2021-01-07 10:19:49 +0800

    leetcode周赛题目，未通过5245

---
+++ src/test/problemAndSolving/leetcode_20201213WeekRankList/T5245堆叠长方体的最大高度Test.java
@@ -0,0 +1,20 @@
+package test.problemAndSolving.leetcode_20201213WeekRankList;
+
+import junit.framework.TestCase;
+import main.problemAndSolving.leetcode_20201213WeekRankList.T5245堆叠长方体的最大高度;
+
+public class T5245堆叠长方体的最大高度Test extends TestCase {
+    T5245堆叠长方体的最大高度 exam;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        exam = new T5245堆叠长方体的最大高度();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testMaxHeight() {
+        assertEquals(190, exam.maxHeight(new int[][]{{50, 45, 20}, {95, 37, 53}, {45, 23, 12}}));
+    }
+}
