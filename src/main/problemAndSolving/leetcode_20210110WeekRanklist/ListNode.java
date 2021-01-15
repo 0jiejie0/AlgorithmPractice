e9a491164c57b63ff51b9d66ee143b6a9405b76d
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2021-01-15 11:10:41 +0800

    leetcode周赛，赛时通过两道题目5649 5652

---
+++ src/main/problemAndSolving/leetcode_20210110WeekRanklist/ListNode.java
@@ -0,0 +1,18 @@
+package main.problemAndSolving.leetcode_20210110WeekRanklist;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
