266e00a2dd565ff504d84d41e9544e18612f3453
0jiejie0 <36239770+0jiejie0@users.noreply.github.com>
2024-03-19 23:47:42 +0800

    添加 leetcode辅助数据结构 访问控制调整

--- src/main/customUtil/leetcode/ListNode.java
+++ src/main/customUtil/leetcode/ListNode.java
@@ -4,17 +4,17 @@ package main.customUtil.leetcode;
  * leetcode辅助数据结构
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
