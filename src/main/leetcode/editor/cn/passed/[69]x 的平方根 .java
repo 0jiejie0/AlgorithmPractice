1b49e4c379846e65c1d3ee194dbf047f8b28858f
0jiejie0 <xkm.0jiejie0@qq.com>
2024-03-27 13:15:12 +0800

    [69]x 的平方根 牛顿迭代法，用一个变量实现，应该是最优了，摸鱼时间做个题正好，不易被发现，下班的整片时间用来看视频学习技术应该是个更优策略

--- src/main/leetcode/editor/cn/passed/[69]x 的平方根 .java
+++ src/main/leetcode/editor/cn/passed/[69]x 的平方根 .java
@@ -36,26 +36,21 @@ package main.leetcode.editor.cn;//给你一个非负整数 x ，计算并返回 
 class Solution {
     /**
      * 印象当中曾经在什么地方看到过平方根的解法，是用的递归，然而具体算法想不起来了
-     * 题目说是二分那就用二分试试吧
+     * 题目说是二分那就用二分试试吧，二分效率有点低，向结果逼近的速度不够快
+     * 看完别人的题解，感觉是自己脑子不太灵光了，结合b=a^2-x函数图像和b'=2a的切线和a轴交点，易得a2=(a1+x/a1)/2点比a1点更为接近实根
+     * (b为因变量，a为自变量)
+     * 递归当然要改成递推，减少出入栈操作
      */
     public int mySqrt(int x) {
-        if (x == 0) return 0;
-        if (x == 1) return 1;
-        int l = 0, h = x;
-        long c;
-        while (l < h) {
-            if (((c = (l + (h - l) >> 1)) * c > Integer.MAX_VALUE) || c * c > x) {
-                h = (int) c;
-            } else if (c * c < x) {
-                l = (int) c + 1;
-            } else if (c * c == x) {
-                return (int) c;
-            }
-        }
-        return l - 1;
+        double a = (x + 1.0) / 2;
+        while ((int) a != (int) (a = (a + x / a) / 2)) ;
+        return (int) a;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 // 	执行耗时:1 ms,击败了88.60% 的Java用户
 //	内存消耗:39.8 MB,击败了61.34% 的Java用户
 // 感觉二分效率有点低，向结果逼近的速度不够快
+// 用牛顿迭代法，时间是降下来了，但是可能是因为两个变量？耗内存并不低，不清楚是不是强转导致的，缩减变量，多次运行，取个最好结果
+//	执行耗时:0 ms,击败了100.00% 的Java用户
+//	内存消耗:39.7 MB,击败了87.20% 的Java用户
