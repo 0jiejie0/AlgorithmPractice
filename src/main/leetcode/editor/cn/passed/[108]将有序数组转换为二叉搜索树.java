a01c2660a52b6084b5a525df28e3d40bc7a4155e
0jiejie0 <xkm.0jiejie0@qq.com>
2024-04-08 10:38:06 +0800

    [108]将有序数组转换为二叉搜索树

---
+++ src/main/leetcode/editor/cn/passed/[108]将有序数组转换为二叉搜索树.java
@@ -0,0 +1,69 @@
+package main.leetcode.editor.cn;//给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：nums = [-10,-3,0,5,9]
+//输出：[0,-3,9,-10,null,5]
+//解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+//
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：nums = [1,3]
+//输出：[3,1]
+//解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 按 严格递增 顺序排列 
+// 
+//
+// Related Topics 树 二叉搜索树 数组 分治 二叉树 👍 1495 👎 0
+
+import main.customUtil.leetcode.TreeNode;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start >= end) return null;
+        if ((start + 1) == end) return new TreeNode(nums[start]);
+        int m = (start + end) >> 1;
+        return new TreeNode(nums[m], sortedArrayToBST(nums, start, m), sortedArrayToBST(nums, m + 1, end));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+// 递归处理相当方便了
+// 	执行耗时:0 ms,击败了100.00% 的Java用户
+//	内存消耗:42 MB,击败了87.46% 的Java用户
