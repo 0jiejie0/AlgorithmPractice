7958b54585051a7df714800c1b4465849bcbb3eb
0jiejie0 <xkm.0jiejie0@qq.com>
2024-06-16 12:09:21 +0800

    第 402 场周赛

---
+++ src/main/problemAndSolving/leetcode_20240616WeekRankList/T100301构成整天的下标对数目II.java
@@ -0,0 +1,51 @@
+package main.problemAndSolving.leetcode_20240616WeekRankList;
+
+// 给你一个整数数组 hours，表示以 小时 为单位的时间，返回一个整数，表示满足 i < j 且 hours[i] + hours[j] 构成 整天 的下标对 i, j 的数目。
+//
+//整天 定义为时间持续时间是 24 小时的 整数倍 。
+//
+//例如，1 天是 24 小时，2 天是 48 小时，3 天是 72 小时，以此类推。
+//
+//
+//
+//示例 1：
+//
+//输入： hours = [12,12,30,24,24]
+//
+//输出： 2
+//
+//解释：
+//
+//构成整天的下标对分别是 (0, 1) 和 (3, 4)。
+//
+//示例 2：
+//
+//输入： hours = [72,48,24,3]
+//
+//输出： 3
+//
+//解释：
+//
+//构成整天的下标对分别是 (0, 1)、(0, 2) 和 (1, 2)。
+//
+//
+//
+//提示：
+//
+//1 <= hours.length <= 5 * 105
+//1 <= hours[i] <= 109
+public class T100301构成整天的下标对数目II {
+    public long countCompleteDayPairs(int[] hours) {
+        int[] cache = new int[24];
+        for (int i = 0; i < hours.length; i++) {
+            cache[hours[i] % 24]++;
+        }
+        long ans = (((long) cache[0]) * (cache[0] - 1)) >> 1;
+        ans += (((long) cache[12]) * (cache[12] - 1)) >> 1;
+        for (int i = 1; i < 12; i++) {
+            ans += cache[i] * cache[24 - i];
+        }
+        return ans;
+
+    }
+}
