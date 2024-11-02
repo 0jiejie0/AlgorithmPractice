bf82c543639b71b3ec26bf2bd6df12bf7212ea6c
0jiejie0 <xkm.0jiejie0@qq.com>
2024-11-02 15:00:59 +0800

    [144]二叉树的前序遍历 常做常新

--- src/main/leetcode/editor/cn/passed/[144]二叉树的前序遍历.java
+++ src/main/leetcode/editor/cn/passed/[144]二叉树的前序遍历.java
@@ -54,8 +54,10 @@ package main.leetcode.editor.cn;//给你二叉树的根节点 root ，返回它�
 
 import main.customUtil.leetcode.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -107,7 +109,42 @@ class Solution {
         }
         return ansList;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
 //	执行耗时:0 ms,击败了100.00% 的Java用户
 //	内存消耗:40.7 MB,击败了56.79% 的Java用户
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> nodeStack = new Stack<>();
+        ArrayList<Integer> ansList = new ArrayList<>();
+        if (null == root) {
+            return ansList;
+        }
+        ansList.add(root.val);
+        nodeStack.push(root);
+        while (!nodeStack.isEmpty()) {
+            root = nodeStack.peek();
+            if (root.left != null) {
+                ansList.add(root.left.val);
+                nodeStack.push(root.left);
+                root.left = null; // 断开已入栈的节点连接，防止重复
+                continue;
+            }
+            if (root.right != null) {
+                ansList.add(root.right.val);
+                nodeStack.push(root.right);
+                root.right = null;
+                continue;
+            }
+            nodeStack.pop();
+        }
+        return ansList;
+    }
+
+// 	执行耗时:0 ms,击败了100.00% 的Java用户
+//	内存消耗:40.6 MB,击败了91.33% 的Java用户
+// 和半年之前做的是两个思路，之前的思路是利用last指针记录上次遍历的结点，处理过程复杂一点，但是不会对原数据产生影响；
+// 这次的思路是直接断开入栈节点的连接，过程更简单粗暴，但会打散原数据结构
+
+// 这次虽然处理过程更简单，答题通过速度也更快，但是却忘了之前last指针的做法，看来还是得常刷，常做常新，今天的自己不一定就比昨天的自己更好，求学真的是逆水行舟啊
+}
+//leetcode submit region end(Prohibit modification and deletion)
