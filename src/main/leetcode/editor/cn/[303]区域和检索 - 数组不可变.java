1ca356099e8a823daeffaa69b32bfc470dd23d02
0jiejie0 <xkm.0jiejie0@qq.com>
2025-06-23 23:26:35 +0800

    [303]区域和检索 - 数组不可变 效率有待提高

---
+++ src/main/leetcode/editor/cn/[303]区域和检索 - 数组不可变.java
@@ -0,0 +1,77 @@
+package main.leetcode.editor.cn;//给定一个整数数组 nums，处理以下类型的多个查询:
+//
+// 
+// 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right 
+// 
+//
+// 实现 NumArray 类： 
+//
+// 
+// NumArray(int[] nums) 使用数组 nums 初始化对象 
+// int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和 
+//right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] ) 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：
+//["NumArray", "sumRange", "sumRange", "sumRange"]
+//[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+//输出：
+//[null, 1, -1, -3]
+//
+//解释：
+//NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+//numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+//numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+//numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 0 <= i <= j < nums.length 
+// 最多调用 10⁴ 次 sumRange 方法 
+// 
+//
+// Related Topics 设计 数组 前缀和 👍 687 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class NumArray {
+    long[] cache;
+
+    public NumArray(int[] nums) {
+        cache = new long[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                cache[j] += nums[i];
+            }
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return (int) (right == cache.length - 1 ? cache[left] : (cache[left] - cache[right + 1]));
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+// 	执行耗时:68 ms,击败了5.12% 的Java用户
+//	内存消耗:48.5 MB,击败了28.24% 的Java用户
+// 这个效率有点低啊？
+// 数据预处理效率确实太慢，平方复杂度，怎么优化呢？
+
