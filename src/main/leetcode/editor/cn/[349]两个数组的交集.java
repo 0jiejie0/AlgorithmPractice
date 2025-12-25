a35b90ebd70cc0c555f96f729adc989fb54e61e5
0jiejie0 <xkm.0jiejie0@qq.com>
2025-12-25 14:00:22 +0800

    [349]两个数组的交集

---
+++ src/main/leetcode/editor/cn/[349]两个数组的交集.java
@@ -0,0 +1,64 @@
+//给定两个数组 nums1 和 nums2 ，返回 它们的 交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,2,1], nums2 = [2,2]
+//输出：[2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出：[9,4]
+//解释：[4,9] 也是可通过的
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 1000 
+// 
+//
+// Related Topics 数组 哈希表 双指针 二分查找 排序 👍 1008 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int c = 1001;
+        int[] cache = new int[c];
+        int n = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            cache[nums1[i]] = 1;
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            int i1 = nums2[i];
+            if (cache[i1] > 0) {
+                cache[i1]++;
+                if (cache[i1] == 2) {
+                    n++;
+                }
+            }
+        }
+        int[] ans = new int[n];
+        int j = 0;
+        for (int i = 0; i < c; i++) {
+            if (cache[i] > 1) {
+                ans[j++] = i;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+// 	执行耗时:2 ms,击败了90.24% 的Java用户
+//	内存消耗:44 MB,击败了49.28% 的Java用户
+// 如果用集合，效率不见得会更高
